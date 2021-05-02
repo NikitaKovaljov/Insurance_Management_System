ab3bd4af5d3345cae97065f149f19f673b172ee2
nikiko <nikiko@ttu.ee>
2021-05-02 18:26:06 +0000

    minor fix

--- includes/logOut.php
+++ includes/logOut.php
@@ -3,11 +3,11 @@ session_name("PHP-PART"); // session name
 session_start(); // session start 
 session_unset(); // unset all cookies 
 session_destroy(); // destroy session 
-setcookie("trafficSelected", "", time()-3600); // delete cookie of trafficSelected
-setcookie("cascoSelected", "", time()-3600); // delete cookie of cascoSelected
-setcookie("lifeSelected", "", time()-3600); // delete cookie of lifeSelected
-setcookie("rememberme", "", time()-3600); // delete "remember me" cookie
-setcookie("homeSelected", "", time()-3600); // delete cookie of homeSelected
+setcookie("traffic_selected", "", time()-3600); // delete cookie of trafficSelected
+setcookie("casco_selected", "", time()-3600); // delete cookie of cascoSelected
+setcookie("life_selected", "", time()-3600); // delete cookie of lifeSelected
+setcookie("rememberme", "", time()-3600, "/"); // delete "remember me" cookie
+setcookie("home_selected", "", time()-3600); // delete cookie of homeSelected
 header("Location: ../index.php"); // redirect user after logout to index.php
 die;
 ?>
