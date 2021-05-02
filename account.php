e6a009daeec40b9943ca864d7328d6f59bacc115
nikiko <nikiko@ttu.ee>
2021-05-02 18:27:12 +0000

    minor fix

--- account.php
+++ account.php
@@ -6,7 +6,7 @@
 session_name("PHP-PART");
 session_start();
 // connection to mysql database
-include "../insurance_management_system/includes/functions.php";
+include "includes/functions.php";
 include_once "DB/connect.db.php";// variables with credentials for connection to db
 
 //Connect to DB
@@ -73,23 +73,23 @@ $data = mysqli_fetch_assoc($result);
             <article>
                 <p style="text-align:center"> <?php if(isset($_SESSION['username'])){echo "Welcome: " .$_SESSION['username'];} ?></p>
                 <button type="button" class="collapsible">Account Details</button>
-                <div class="account_details">
-                    <div class="current_data">
+                <div class="account-details">
+                    <div class="current-data">
                         <p>Your full name: <?php echo $data['full_name']?> </p><br>
                         <p>Your ID-Code: <?php echo $data['personal_code']?> </p><br>
                         <p>Your current email: <?php echo $data['email']?> </p><br>
                         <p>Your current phone number: <?php echo $data['phone_number']?> </p>
                     </div>
-                    <form onsubmit="return validateDetails()" action = "includes/detailsLogic.php" method = "POST" id="detailsForm">
-                        <label for="newPass">New Password:</label>
-                        <input type="password" id="newPass" name="newPass" minlength="8">
-                        <input type="submit" name="newPassSubmit" id="newPassSubmit" value="Change Password"><br>
-                        <label for="newEmail">New Email:</label>
-                        <input type="email" id="newEmail" name="newEmail">
-                        <input type="submit" name="newEmailSubmit" id="newEmailSubmit" value="Change Email"><br>
-                        <label for="newNumber">New Phone Number:</label>
-                        <input type="tel" id="newNumber" name="newNumber" pattern="[0-9+-]{1,7}[0-9]{6,9}">
-                        <input type="submit" name="newNumberSubmit" id="newNumberSubmit" value="Change Phone Number">
+                    <form onsubmit="return validate_details()" action = "includes/details_logic.php" method = "POST" id="details_form">
+                        <label for="new_pass">New Password:</label>
+                        <input type="password" id="new_pass" name="new_pass" minlength="8">
+                        <input type="submit" name="new_pass_submit" id="new_pass_submit" value="Change Password"><br>
+                        <label for="new_email">New Email:</label>
+                        <input type="email" id="new_email" name="new_email">
+                        <input type="submit" name="new_email_submit" id="new_email_submit" value="Change Email"><br>
+                        <label for="new_number">New Phone Number:</label>
+                        <input type="tel" id="new_number" name="new_number" pattern="[0-9+-]{1,7}[0-9]{6,9}">
+                        <input type="submit" name="new_number_submit" id="new_number_submit" value="Change Phone Number">
                     </form>    
                     </div>                
                 <h1>Your Active Services</h1>
@@ -105,9 +105,9 @@ $data = mysqli_fetch_assoc($result);
                         </tr>
                         <?php
                             // output table with user services
-                            $table_Casco = "SELECT username, valid_till, payment, license_plate, power FROM Project_Service_Casco WHERE username='$username';";
+                            $table_casco = "SELECT username, valid_till, payment, license_plate, power FROM Project_Service_Casco WHERE username='$username';";
 
-                            $search_result = mysqli_query($mysqli, $table_Casco);
+                            $search_result = mysqli_query($mysqli, $table_casco);
 
                             if(mysqli_num_rows($search_result) > 0){
                                 while($row = mysqli_fetch_assoc($search_result)){
@@ -120,9 +120,9 @@ $data = mysqli_fetch_assoc($result);
                                 }
                             }
                             
-                            $table_Home = "SELECT username, valid_till, payment, area, material FROM Project_Service_Home WHERE username='$username';";
+                            $table_home = "SELECT username, valid_till, payment, area, material FROM Project_Service_Home WHERE username='$username';";
 
-                            $search_result = mysqli_query($mysqli, $table_Home);
+                            $search_result = mysqli_query($mysqli, $table_home);
 
                             if(mysqli_num_rows($search_result) > 0){
                                 while($row = mysqli_fetch_assoc($search_result)){
@@ -135,9 +135,9 @@ $data = mysqli_fetch_assoc($result);
                                 }
                             }
 
-                            $table_Life = "SELECT username, valid_till, payment, age, income FROM Project_Service_Life WHERE username='$username';";
+                            $table_life = "SELECT username, valid_till, payment, age, income FROM Project_Service_Life WHERE username='$username';";
 
-                            $search_result = mysqli_query($mysqli, $table_Life);
+                            $search_result = mysqli_query($mysqli, $table_life);
 
                             if(mysqli_num_rows($search_result) > 0){
                                 while($row = mysqli_fetch_assoc($search_result)){
@@ -150,9 +150,9 @@ $data = mysqli_fetch_assoc($result);
                                 }   
                             }
 
-                            $table_Traffic = "SELECT username, valid_till, payment, license_plate, power FROM Project_Service_Traffic WHERE username='$username';";
+                            $table_traffic = "SELECT username, valid_till, payment, license_plate, power FROM Project_Service_Traffic WHERE username='$username';";
 
-                            $search_result = mysqli_query($mysqli, $table_Traffic);
+                            $search_result = mysqli_query($mysqli, $table_traffic);
 
                             if(mysqli_num_rows($search_result) > 0){
                                 while($row = mysqli_fetch_assoc($search_result)){
